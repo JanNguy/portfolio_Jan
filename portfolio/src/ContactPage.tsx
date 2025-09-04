c88ecdde9e26d60eea76790b330379a00ff6a5c3
Jan <jan.nguyen69400@gmail.com>
2025-09-04 23:33:37 +0200

    add: better orga and base disign

--- portfolio/src/ContactPage.tsx
+++ portfolio/src/ContactPage.tsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom";
 import './App.css'
 
 function ContactPage() {
     return (
         <>
-            <p>contact page</p>
+            <div className='flex flex-nowrap flex-col justify-start items-start m-7'>
+                <p className='edwarian text-9xl -ml-5'>Contacts</p>
+
+                <section className='mt-4 border-1 border-solid border-stone-600 mx-7 p-2'> {/*si une des sections est hover alors la preview du lien se joue*/}
+                    <div className='flex flex-row gap-4 mb-4'>
+                        <img src="../public/linkedin-logo.svg" alt="linkedin-logo" className='w-8 h-8 transition duration-150 ease-out hover:translate-1'/>
+                        <img src="../public/linkedin-logo.svg" alt="linkedin-logo" className='w-8 h-8 transition duration-150 ease-out hover:translate-1'/>
+                        <img src="../public/linkedin-logo.svg" alt="linkedin-logo" className='w-8 h-8 transition duration-150 ease-out hover:translate-1'/>
+                    </div>
+                    <div className='flex flex-row gap-4'>
+                        <img src="../public/linkedin-logo.svg" alt="linkedin-logo" className='w-8 h-8 transition duration-150 ease-out hover:translate-1'/>
+                        <img src="../public/linkedin-logo.svg" alt="linkedin-logo" className='w-8 h-8 transition duration-150 ease-out hover:translate-1'/>
+                        <img src="../public/linkedin-logo.svg" alt="linkedin-logo" className='w-8 h-8 transition duration-150 ease-out hover:translate-1'/>
+                    </div>
+                </section>
+                <div className="flex flex-row flex-nowrap items-center ml-5 transition duration-150 ease-out hover:translate-x-1 no-underline">
+                    <p className="mt-3">←</p>
+                    <Link to="/" className='edwarian mt-6 ml-2 text-5xl hover:underline decoration-[0.25px] underline-offset-5'>Back</Link>
+                </div>
+
+            </div>
         </>
     )
 }
--- portfolio/src/Home.tsx
+++ portfolio/src/Home.tsx
@@ -9,37 +9,46 @@ function Home() {
                     <p className="edwarian text-9xl">Jan Nguyen</p>
                     <p className="edwarian text-5xl">French developer</p>
                 </div>
-                <div className="bg-white flex justify-end fixed top-0 right-0 items-center h-full w-auto border-solid border-1 border-black">
+                <div className="bg-white flex justify-end fixed top-0 right-0 items-center h-full w-[20vh] border-solid border-1 border-black"> {/*apparait en scrollant quand l'affichage est sur la 2eme "page". Quand chaque li est hover une "preview se lance & add event listener sur si la section est hover pour affihcer un point avant" */}
                     <ul className="mr-6 space-y-3 list-none text-right">
                         <li>
-                        <Link className="navBar" to="/">
-                            <p className="text-1xl sm:text-2xl md:text-5xl lg:text-6xl xl:text-6xl leading-tight transition duration-150 ease-out hover:translate-x-1 no-underline hover:underline decoration-[0.25px] underline-offset-5">
-                            Home
+                        <Link to="/">
+                            <p className="edwarian text-lg sm:text-xl md:text-4xl lg:text-5xl xl:text-5xl leading-tight transition duration-150 ease-out hover:-translate-x-1 no-underline hover:underline decoration-[0.25px] underline-offset-5 before:content-['']">
+                                Home {/*reset la pos à la "page 1" */}
                             </p>
                         </Link>
                         </li>
                         <li>
-                        <Link className="navBar" to="ContactPage">
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight transition duration-150 ease-out hover:translate-x-1 no-underline hover:underline decoration-[0.25px] hover:underline-offset-5">
-                            Contact
+                        <Link to="ContactPage">
+                            <p className="edwarian text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl leading-tight transition duration-150 ease-out hover:-translate-x-1 no-underline hover:underline decoration-[0.25px] hover:underline-offset-5">
+                                Contact
                             </p>
                         </Link>
                         </li>
                         <li>
-                        <Link className="navBar" to="IndexPage">
-                            <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight transition duration-150 ease-out hover:translate-x-1 no-underline hover:underline decoration-[0.25px] underline-offset-5">
-                            Projects
+                        <Link to="IndexPage">
+                            <p className="edwarian text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl leading-tight transition duration-150 ease-out hover:-translate-x-1 no-underline hover:underline decoration-[0.25px] underline-offset-5">
+                                Projects
                             </p>
                         </Link>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div className="h-screen bg-gray-100 flex justify-center items-center"> {/* mettre en fond un effet 3D de nuage ou quelque chose du genre à effet de particle */}
-                <div className="flex flex-col text-left justify-center bg-slate-100 rounded w-150 align-middle h-50">
+            <div className="h-screen bg-gray-100 flex justify-center items-center w-auto"> {/* mettre en fond un effet 3D de nuage ou quelque chose du genre à effet de particle */}
+                <div className="h-full bg-gray-100 flex justify-center items-center w-auto">
+                    <p className="montHeavy text-7xl">Start</p>
+                </div>
+                {/* <div className="flex flex-col text-left justify-center bg-slate-100 rounded w-150 align-middle h-50">
                     <p className="text-4xl">Who I am?</p>
                     <p>A french student at Epitech Lyon.<br/> I am a deep dreamer. Passionate and tinkerer, I love experimenting in new areas.</p>
+                </div> */}
+            </div>
+            <div>
+                <div className="h-screen w-auto bg-white flex justify-center items-center">
+                    <p>placeholder</p>
                 </div>
+                {/*footer qui apparait en scrollant*/}
             </div>
         </body>
     </>
