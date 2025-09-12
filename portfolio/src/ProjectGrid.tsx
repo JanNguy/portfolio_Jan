a834e502c0ebbd454948e8838e3f279b5af0e41d
Jan <jan.nguyen69400@gmail.com>
2025-09-12 20:24:58 +0200

    add: vercel file, img for project + fix: grid

--- portfolio/src/ProjectGrid.tsx
+++ portfolio/src/ProjectGrid.tsx
@@ -15,28 +15,28 @@ const projects: ProjectBase[] = [
     {
         id: 1,
         title: "BlockApp",
-        pathImg: "/linkedin-logo.svg",
+        pathImg: "/blockImg.png",
         description: "My first react project. Its a simple moving blocks project",
         url: `${baseUrl}/blockFristApp`,
     },
     {
         id: 2,
         title: "DeepSearch",
-        pathImg: "/linkedin-logo.svg",
+        pathImg: "/DeepSearch.jpeg",
         description: "A Deepseek-R1 based deepsearch function made for an 4hours Hackathon.",
         url: "https://github.com/JanNguy/DeepSearch",
     },
     {
         id: 3,
         title: "FastStart",
-        pathImg: "/linkedin-logo.svg",
+        pathImg: "/FastStart.png",
         description: "A C project luncher for with custom header and start patterns.",
         url: "https://github.com/JanNguy/FastStart",
     },
     {
         id: 4,
         title: "cross-easy-trial",
-        pathImg: "/linkedin-logo.svg",
+        pathImg: "/Crossoverfreetrial.png",
         description: "A script to bypass the end of the free trial indefinitely",
         url: "https://github.com/JanNguy/cross-easy-trial",
     },
@@ -45,8 +45,8 @@ const projects: ProjectBase[] = [
 function ProjectCard({ title, pathImg, description, url }: ProjectBase) {
     return (
         <div className="p-5 h-[60vh] w-[40vh] border border-stone-600 rounded flex flex-col">
-            <div className="h-[40%] w-full border flex items-center justify-center overflow-hidden">
-                <img className="w-full h-full object-cover" src={pathImg} alt={title} />
+            <div className="h-[40%] w-full border flex items-center justify-center">
+                <img className="max-h-full w-auto object-contain" src={pathImg} alt={title} />
             </div>
             <div className="h-[60%] mt-2 flex flex-col justify-between">
                 <div>
