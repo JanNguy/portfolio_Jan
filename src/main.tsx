131f18438c6d917a42efca7c18ecd2e03f9f675d
Jan Nguyen <jan.nguyen@epitech.eu>
2025-10-20 04:31:37 +0200

    fix: vercel tracking

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./client/App.tsx";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <App />
+        <Analytics />
         <SpeedInsights />
     </StrictMode>,
 );
